0693f4f5dd180735086a4142306004d2fc0f17e9
Martha Staus <mjstaus@gmail.com>
2022-10-16 13:04:08 -0700

    initial commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
